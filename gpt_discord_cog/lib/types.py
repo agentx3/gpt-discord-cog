cc5ad37d015cd329f2ab8723a2ba6fc265026bde
agentx3 <thepuzzlegang.agent@gmail.com>
2023-11-12 15:27:34 -0800

    Added extra types for config

--- gpt_discord_cog/lib/types.py
+++ gpt_discord_cog/lib/types.py
@@ -1,10 +1,45 @@
 from typing import (
+    Callable,
+    Optional,
     TypedDict,
 )
+from discord import ApplicationContext
 
 from openai import AsyncOpenAI
-from openai.types.beta import Assistant, Thread
 import sqlite3
+
+
+class UserCommandOptions(TypedDict, total=False):
+    # This should be compatible with the rules for a discord command name
+    name: Optional[str]
+    description: Optional[str]
+    check: Callable[[ApplicationContext], bool]
+
+
+class UserCommandConfig(TypedDict, total=False):
+    modify: UserCommandOptions
+
+
+class CommandOptions(TypedDict):
+    # This should be compatible with the rules for a discord command name
+    name: str
+    description: str
+    check: Callable[[ApplicationContext], bool]
+
+
+class CommandConfig(TypedDict):
+    modify: CommandOptions
+
+
+class UserGPTConfig(TypedDict):
+    """The only purpose for this is to make some of the keys optional"""
+
+    client: AsyncOpenAI
+    assistant_id: str
+    database_connection: sqlite3.Connection  # TODO: Add a handler for redis
+    database_name: str
+    conversation_lifetime: int
+    commands: Optional[UserCommandConfig]
 
 
 class GPTConfig(TypedDict):
@@ -13,3 +48,4 @@ class GPTConfig(TypedDict):
     database_connection: sqlite3.Connection  # TODO: Add a handler for redis
     database_name: str
     conversation_lifetime: int
+    commands: CommandConfig
